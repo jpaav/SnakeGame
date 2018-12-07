afeb58853d6056a2a244812512689cd622781a05
jpaav <joepaavola@gmail.com>
2018-12-07 17:09:02 -0500

    Added basic draw functions and text rendering for the title screen

--- logic/game.py
+++ logic/game.py
@@ -7,12 +7,12 @@ class SnakeGame:
 	should_quit = False
 	height = 600
 	width = 600
-	screen = States()
+	state = States(0)
+	screen = pg.display.set_mode((width, height))
+	font = None
 
 	def start(self):
-		#
 		# Init PyGame
-		#
 		(passed, failed) = pg.init()
 		print("Number of modules successfully loaded: " + str(passed))
 		print("Number of modules failed to load: " + str(failed))
@@ -21,10 +21,11 @@ class SnakeGame:
 			return 1
 		# Create screen with given dimensions
 		pg.display.set_mode((self.width, self.height))
-		#
+		pg.display.set_caption("Staterpillar")
+		self.font = pg.font.SysFont('Comic Sans MS', 30)
 		# Init game logic
-		#
-		self.screen = States.TITLE
+		self.state = States.TITLE
+
 		# Loop until should_quit is changed to true
 		while not self.should_quit:
 			self.loop()
@@ -33,7 +34,8 @@ class SnakeGame:
 	def loop(self):
 		self.get_keyboard_input()
 		self.check_quit()
-		pg.display.flip()
+		self.draw()
+		pg.display.update()
 
 	def check_quit(self):
 		for event in pg.event.get():
@@ -46,6 +48,29 @@ class SnakeGame:
 			self.should_quit = True
 		return
 
+	# Draw functions
 
+	def draw(self):
+		if self.state == States.TITLE:
+			self.draw_title()
+		elif self.state == States.GAME:
+			self.draw_game()
+		elif self.state == States.SCORE:
+			self.draw_score()
+		elif self.state == States.DEAD:
+			self.draw_dead()
 
+	def draw_title(self):
+		# TODO: Replace this with a picture logo Ethan made
+		self.screen.fill([0, 200, 0])
+		title_text = self.font.render('Staterpillar', True, (255, 255, 255))
+		self.screen.blit(title_text, ((self.width/2) - title_text.get_rect().width/2, 10))
 
+	def draw_game(self):
+		pass
+
+	def draw_score(self):
+		pass
+
+	def draw_dead(self):
+		pass
